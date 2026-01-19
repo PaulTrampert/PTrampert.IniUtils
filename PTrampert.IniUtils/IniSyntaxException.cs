1ce75e94b0c8815d063fb359d65289ab6fc99ca4
Paul Trampert <paul.trampert@ptrampert.com>
2026-01-19 17:16:16 -0500

    Fix typo

--- PTrampert.IniUtils/IniSyntaxException.cs
+++ PTrampert.IniUtils/IniSyntaxException.cs
@@ -14,11 +14,11 @@ public class IniSyntaxException : Exception
     /// <param name="lineContent">The content of the line.</param>
     /// <param name="filePath">The file the error occurred in, if a file.</param>
     public IniSyntaxException(int lineNumber, string lineContent, string? filePath)
-        : base($"Syntax error{FileMessasgeClause(filePath)} at line {lineNumber}: '{lineContent}'")
+        : base($"Syntax error{FileMessageClause(filePath)} at line {lineNumber}: '{lineContent}'")
     {
     }
 
-    private static string FileMessasgeClause(string? filePath)
+    private static string FileMessageClause(string? filePath)
     {
         return filePath == null ? string.Empty : $" in '{filePath}'";
     }
