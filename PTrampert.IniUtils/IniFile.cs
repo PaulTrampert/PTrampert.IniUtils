d20b5978674545835073159820c13ba2a3b68140
Paul Trampert <paul.trampert@ptrampert.com>
2026-01-19 11:01:28 -0500

    Add documentation comments

--- PTrampert.IniUtils/IniFile.cs
+++ PTrampert.IniUtils/IniFile.cs
@@ -2,7 +2,14 @@ using System.Collections.Generic;
 
 namespace PTrampert.IniUtils;
 
+/// <summary>
+/// Data structure representing an INI file.
+/// </summary>
 public class IniFile
 {
+    /// <summary>
+    /// The sections in the INI file, keyed by section name. The top-level (global) section
+    /// is represented by an empty string as the key.
+    /// </summary>
     public Dictionary<string, IniSection> Sections { get; } = new();
 }
